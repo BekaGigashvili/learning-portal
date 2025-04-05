ba77cb81db1a2815578192c5c7d60e525b03ac8f
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-06 02:43:24 +0400

    creating registrationRequest

---
+++ src/main/java/com/javaprojects/learningportal/model/RegistrationRequest.java
@@ -0,0 +1,17 @@
+package com.javaprojects.learningportal.model;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class RegistrationRequest {
+    private final String fistName;
+    private final String lastName;
+    @Email
+    private final String email;
+    private final String password;
+}
