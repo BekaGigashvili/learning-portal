c3f4579d8b1f09891098b7a5d5f86c912b51d35a
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-07 01:36:33 +0400

    changing WebSecurityConfig

--- src/main/java/com/javaprojects/learningportal/security/config/WebSecurityConfig.java
+++ src/main/java/com/javaprojects/learningportal/security/config/WebSecurityConfig.java
@@ -1,15 +1,21 @@
 package com.javaprojects.learningportal.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final JwtRequestFilter jwtRequestFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -19,7 +25,9 @@ public class WebSecurityConfig {
                                 .requestMatchers("/registration/**")
                                 .permitAll()
                                 .anyRequest().authenticated()
-                );
+                ).exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(authenticationEntryPoint))
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
