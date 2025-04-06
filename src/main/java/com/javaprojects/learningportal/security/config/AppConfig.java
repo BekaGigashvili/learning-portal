77478d7913fc71420fbe2c9c6b62c1c8e44c216d
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-07 02:12:27 +0400

    creating AppConfig and deleting PasswordEcoder class

---
+++ src/main/java/com/javaprojects/learningportal/security/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.javaprojects.learningportal.security.config;
+
+import com.javaprojects.learningportal.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+    private final UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository
+                .findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
