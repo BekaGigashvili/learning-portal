d1553c7e48f4e334186f5480257fa65c9857719a
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-07 01:37:41 +0400

    changing AuthenticationService

--- src/main/java/com/javaprojects/learningportal/service/AuthenticationService.java
+++ src/main/java/com/javaprojects/learningportal/service/AuthenticationService.java
@@ -1,4 +1,33 @@
 package com.javaprojects.learningportal.service;
 
+import com.javaprojects.learningportal.model.AuthenticationRequest;
+import com.javaprojects.learningportal.model.AuthenticationResponse;
+import com.javaprojects.learningportal.model.User;
+import com.javaprojects.learningportal.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        String email = request.getEmail();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        String password = request.getPassword();
+        if(passwordEncoder.matches(password, user.getPassword())) {
+            String token = jwtUtil.generateToken(user);
+            return AuthenticationResponse
+                    .builder()
+                    .token(token)
+                    .build();
+        }else {
+            throw new RuntimeException("Wrong password");
+        }
+    }
 }
