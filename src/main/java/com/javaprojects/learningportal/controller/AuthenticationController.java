a8b723803aaee7b6b3c6376cc06256b0f672a893
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-07 02:13:25 +0400

    creating authentication controller

---
+++ src/main/java/com/javaprojects/learningportal/controller/AuthenticationController.java
@@ -0,0 +1,25 @@
+package com.javaprojects.learningportal.controller;
+
+import com.javaprojects.learningportal.model.AuthenticationRequest;
+import com.javaprojects.learningportal.model.AuthenticationResponse;
+import com.javaprojects.learningportal.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/authentication")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+    }
+
+}
