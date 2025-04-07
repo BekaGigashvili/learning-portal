7151f9ceeb6ed60f2c4e71ffc5344027f07f05a8
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-08 02:41:21 +0400

    creating course response

---
+++ src/main/java/com/javaprojects/learningportal/model/CourseResponse.java
@@ -0,0 +1,21 @@
+package com.javaprojects.learningportal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseResponse {
+    private String name;
+    private String description;
+    private String thumbnailURL;
+    private double price;
+    private String instructorEmail;
+    private List<String> lessonTitles;
+}
