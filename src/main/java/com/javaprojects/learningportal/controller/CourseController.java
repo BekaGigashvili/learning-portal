a3cb283c40e94e28bd6661e757d7f12efb18d705
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-07 22:21:50 +0400

    adding createCourse method endpoint

--- src/main/java/com/javaprojects/learningportal/controller/CourseController.java
+++ src/main/java/com/javaprojects/learningportal/controller/CourseController.java
@@ -1,13 +1,14 @@
 package com.javaprojects.learningportal.controller;
 
+import com.javaprojects.learningportal.model.Course;
+import com.javaprojects.learningportal.model.CourseRequest;
 import com.javaprojects.learningportal.model.Lesson;
 import com.javaprojects.learningportal.model.User;
 import com.javaprojects.learningportal.service.CourseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -24,5 +25,12 @@ public class CourseController {
     @GetMapping("/students/{courseId}")
     public Set<User> getEnrolledUsers(@PathVariable Long courseId) {
         return courseService.getEnrolledStudents(courseId);
+    }
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('INSTRUCTOR')")
+    public Course createCourse(@RequestBody CourseRequest request,
+                               Authentication authentication) {
+        User instructor = (User) authentication.getPrincipal();
+        return courseService.createCourse(request, instructor);
     }
 }
