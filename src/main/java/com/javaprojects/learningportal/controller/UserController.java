30022bc259706dfca1cf5cfa7f96c4a33734ac6d
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-07 22:20:46 +0400

    changing user controller for better code structure

--- src/main/java/com/javaprojects/learningportal/controller/UserController.java
+++ src/main/java/com/javaprojects/learningportal/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.javaprojects.learningportal.controller;
 
 import com.javaprojects.learningportal.model.Course;
+import com.javaprojects.learningportal.model.User;
 import com.javaprojects.learningportal.service.UserService;
-import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -14,15 +16,17 @@ import java.util.Set;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/enroll/{courseId}/{userId}")
-    public String enrollInCourse(@PathVariable Long courseId,
-                                 @PathVariable Long userId) {
-        return userService.enrollInCourse(courseId, userId);
+    @PostMapping("/enroll/{courseId}")
+    @PreAuthorize("hasRole('STUDENT')")
+    public String enrollInCourse(Authentication authentication,
+                                 @PathVariable Long courseId) {
+        User user = (User) authentication.getPrincipal();
+        return userService.enrollInCourse(courseId, user.getId());
     }
-    @GetMapping("/courses/{userId}")
-    public Set<Course> getEnrolledCourses(@PathVariable Long userId) {
-        return userService.getEnrolledCourses(userId);
+    @GetMapping("/courses")
+    @PreAuthorize("hasRole('STUDENT')")
+    public Set<Course> getEnrolledCourses(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return userService.getEnrolledCourses(user.getId());
     }
-
-
 }
