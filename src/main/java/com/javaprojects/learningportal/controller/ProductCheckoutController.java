89b0e0ec0ae5e1827ae542bb4bca6a66ec4fca4c
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-12 02:19:56 +0400

    creating checkout functionality endpoint

---
+++ src/main/java/com/javaprojects/learningportal/controller/ProductCheckoutController.java
@@ -0,0 +1,36 @@
+package com.javaprojects.learningportal.controller;
+
+import com.javaprojects.learningportal.model.StripeRequest;
+import com.javaprojects.learningportal.model.StripeResponse;
+import com.javaprojects.learningportal.model.User;
+import com.javaprojects.learningportal.service.OrderService;
+import com.javaprojects.learningportal.service.StripeService;
+import com.javaprojects.learningportal.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductCheckoutController {
+
+    private final StripeService stripeService;
+
+    @PostMapping("/checkout")
+    @Transactional
+    public ResponseEntity<StripeResponse> checkout(@RequestBody StripeRequest request,
+                                                   Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        StripeResponse response = stripeService.checkout(request, user);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+}
