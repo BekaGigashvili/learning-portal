fa29d044856f70908e98ea68830fbc4575076ebd
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-12 02:15:56 +0400

    adding order

---
+++ src/main/java/com/javaprojects/learningportal/model/OrderResponse.java
@@ -0,0 +1,23 @@
+package com.javaprojects.learningportal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponse {
+    private Long userId;
+    private Long courseId;
+    private LocalDateTime orderDate;
+    private BigDecimal totalPrice;
+    private String paymentStatus;
+    private String paymentIntentId;
+    private PaymentMethodType paymentMethodType;
+}
