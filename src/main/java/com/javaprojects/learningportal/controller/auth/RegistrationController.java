c585323f1bc1eee69b86a0a91dd929ada77316b5
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-18 06:22:51 +0400

    changing endpoint name

--- src/main/java/com/javaprojects/learningportal/controller/auth/RegistrationController.java
+++ src/main/java/com/javaprojects/learningportal/controller/auth/RegistrationController.java
@@ -2,6 +2,7 @@ package com.javaprojects.learningportal.controller.auth;
 
 import com.javaprojects.learningportal.model.auth.RegistrationRequest;
 import com.javaprojects.learningportal.service.UserService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,25 +10,20 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/registration")
+@RequestMapping("/auth")
 public class RegistrationController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody RegistrationRequest request){
-        try{
-            String response = userService.register(request);
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(response);
-        } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(e.getMessage());
-        }
+    public ResponseEntity<String> registerUser(@RequestBody RegistrationRequest request) throws MessagingException {
+        String response = userService.register(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
+
     @GetMapping("/verify")
-    public String verifyAndEnableUser(@RequestParam("token") String token){
+    public String verifyAndEnableUser(@RequestParam("token") String token) {
         return userService.verifyAndEnableUser(token);
     }
 }
