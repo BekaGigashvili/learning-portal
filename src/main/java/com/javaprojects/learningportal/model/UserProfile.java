d7f8f48981c30f05a7ecd2d6e5c45e578ec3254a
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-18 21:37:13 +0400

    adding UserProfile model

---
+++ src/main/java/com/javaprojects/learningportal/model/UserProfile.java
@@ -0,0 +1,21 @@
+package com.javaprojects.learningportal.model;
+
+import com.javaprojects.learningportal.model.course.CourseResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfile {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String photoUrl;
+    private List<CourseResponse> enrolledCourses;
+}
